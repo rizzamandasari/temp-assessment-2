0733e83fb69e882ab7394063f9ab1daec2d459ee
Rizza Mandasari <646671+rizzamandasari@users.noreply.github.com>
2024-01-02 09:31:43 +0700

    Initial commit

---
+++ Soal02.java
@@ -0,0 +1,6 @@
+public class Soal02 {
+
+	public static void main(final String[] args) {
+		// Kerjakan soalnya di sini
+	}
+}
---
+++ Soal04.java
@@ -0,0 +1,6 @@
+public class Soal04 {
+
+	public static void main(final String[] args) {
+		// Kerjakan soalnya di sini
+	}
+}
